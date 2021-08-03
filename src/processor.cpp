1671835d8ecd554abb68af41ced0235a836c66fb
chimaxcorporations <chimaxcorporations@gmail.com>
2021-08-03 09:58:04 +0000

    Processor Utilization updated

--- src/processor.cpp
+++ src/processor.cpp
@@ -6,7 +6,7 @@
 float Processor::Utilization() { 
 long prevActiveJiffies = LinuxParser::ActiveJiffies();
 long prevIdleJiffies = LinuxParser::IdleJiffies();
-std::this_thread::sleep_for (std::chrono::seconds(5));
+std::this_thread::sleep_for (std::chrono::seconds(1));
 long currentActiveJiffies = LinuxParser::ActiveJiffies();
 long currentIdleJiffies = LinuxParser::IdleJiffies();
 long prevCPUState = prevActiveJiffies/(prevActiveJiffies+prevIdleJiffies);
